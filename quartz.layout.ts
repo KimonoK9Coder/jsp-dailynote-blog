8505efdacd93585d102cad35578e6b271af8c6c2
inderstrybaby <jongseokpark910@gmail.com>
2025-07-14 13:24:14 +0900

    modified second hello world.md

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -1,5 +1,6 @@
-import { PageLayout, SharedLayout } from "./quartz/cfg"
 import * as Component from "./quartz/components"
+
+import { PageLayout, SharedLayout } from "./quartz/cfg"
 
 // components shared across all pages
 export const sharedPageComponents: SharedLayout = {
@@ -41,7 +42,7 @@ export const defaultContentPageLayout: PageLayout = {
     Component.Explorer(),
   ],
   right: [
-    Component.Graph(),
+    // Component.Graph(),
     Component.DesktopOnly(Component.TableOfContents()),
     Component.Backlinks(),
   ],
