97d71a9b1c6f0e7b59543d5cc17a0dc1eec63aeb
inderstrybaby <jongseokpark910@gmail.com>
2025-07-17 12:23:46 +0900

    edit quartz.config.ts

--- quartz.config.ts
+++ quartz.config.ts
@@ -18,7 +18,7 @@ const config: QuartzConfig = {
     },
     locale: "en-US",
     baseUrl: "quartz.jzhao.xyz",
-    ignorePatterns: ["!(blog/*.md)"],
+    ignorePatterns: ["!(blog/**/*.md)"],
     defaultDateType: "modified",
     theme: {
       fontOrigin: "googleFonts",
