9d1762580a6d5f967da2b9f16cc4c3569b26acc3
inderstrybaby <jongseokpark910@gmail.com>
2025-07-19 14:17:14 +0900

    edit quartz.layout.ts

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -53,11 +53,7 @@ export const defaultContentPageLayout: PageLayout = {
     }),
     Component.Explorer(),
   ],
-  right: [
-    Component.Graph(),
-    Component.DesktopOnly(Component.TableOfContents()),
-    Component.Backlinks(),
-  ],
+  right: [Component.Graph(), Component.TableOfContents(), Component.Backlinks()],
 }
 
 // components for pages that display lists of pages  (e.g. tags or folders)
